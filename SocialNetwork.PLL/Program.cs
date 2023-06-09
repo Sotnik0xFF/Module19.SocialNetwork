3c25515a737f3b071c8954bb44fa9e0edb4eaa14
Sotnik0xFF <sotnikoff1987@gmail.com>
2023-06-09 15:37:29 +0500

    Add project files.

---
+++ SocialNetwork.PLL/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialNetwork.PLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
